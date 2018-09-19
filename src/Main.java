eaec231a291e39d492bc1bb45209e28b26511aae
sahil_21 <ssashi2@illinois.edu>
2018-09-18 20:41:22 -0500

    Initial project commit.

--- src/Main.java
+++ src/Main.java
@@ -4,3 +4,5 @@ public class Main {
 	// write your code here
     }
 }
+
+
--- src/Webscrapper.java
+++ src/Webscrapper.java
@@ -1,6 +1,52 @@
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
+
+
 public class Webscrapper {
-    public static void main(String[] unused){
+    /**
+     * Retrieve contents from a URL and return them as a string.
+     *
+     * @param url url to retrieve contents from
+     * @return the contents from the url as a string, or an empty string on error
+     */
+
+    public static int returnsthenooftimesoccurance(final String q){
+        String caler=q;
+
         
+
+
+
+
+
+
+
+    }
+
+
+
+
+    public static String urlToString(final String url) {
+        Scanner urlScanner;
+        try {
+            urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
+        } catch (IOException e) {
+            return "";
+        }
+        String contents = urlScanner.useDelimiter("\\A").next();
+        urlScanner.close();
+        return contents;
+
+    }
+
+    public static void main(String[] unused){
+
+
 
     }
 }
+
+
